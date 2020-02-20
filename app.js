745eaff3fbaa9d0ef1010729b3b984c8f4954256
Vishal <vishal.s.kumar99@gmail.com>
2020-02-20 21:11:52 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,181 @@
+var express = require("express");
+var cookieParser = require('cookie-parser');
+var body_parser = require('body-parser');
+var session = require('express-session');
+var fileops = require("./fileops");
+var fs = require("fs");
+var app = express();
+
+app.use(express.json());
+app.use(body_parser.urlencoded({extended: false}))
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(cookieParser());
+app.use(session({secret: "secret"}));
+
+app.get("/", (req, res) => {
+    if(req.session.user) return res.redirect("/home");
+    return res.redirect("/login");
+});
+
+app.get("/login", (req, res) => {
+    if(req.session.user) return res.redirect("/home");
+    res.render("login");
+});
+
+app.post("/login", (req, res) => {
+    var userData = fileops.getParsedData("data/users.json");
+    userData
+        .then((Data) => {
+            let signedIn = false;
+            for(i of Data) {
+                if(i.username == req.body.username && i.password == req.body.password) {
+                    req.session.user = i;
+                    signedIn = true;
+                    break;
+                }
+            }
+            if(!signedIn) 
+                res.render("login", {
+                    message: "Invalid credentials. Try again."
+                });
+            else
+                return res.redirect("/home");
+        });
+});
+
+app.get("/home", (req, res) => {
+    if(!req.session.user) return res.redirect("/login");
+    var regCourses = [];
+    var authCourses = [];
+    var courseData = fileops.getCourses(req.session.user);
+    courseData
+        .then((data) => {
+            regCourses = data[0];
+            authCourses = data[1];
+            res.render("home", {
+                user: req.session.user,
+                courses: regCourses,
+                auth: authCourses
+            });
+        });
+});
+
+app.post("/home", (req, res) => {
+    req.session.destroy();
+    return res.redirect("/login");
+});
+
+app.get("/profile", (req, res) => {
+    if(!req.session.user) return res.redirect("/login");
+    res.render("profile", {
+        user: req.session.user
+    });
+});
+
+app.get("/course/:id", (req, res) => {
+    if(!req.session.user) return res.redirect("/login");
+    var course = fileops.courseDetails(req.params.id);
+    course
+        .then((data) => {
+            res.render("course", {
+                id: req.params.id,
+                course: data,
+                user: req.session.user
+            });
+        })
+});
+
+app.get("/new_course", (req, res) => {
+    if(!req.session.user) return res.redirect("/login");
+    res.render("new_course");
+});
+
+app.post("/new_course", (req, res) => {
+    id = req.body.id;
+    //First update the course database
+    var courseData = fileops.getParsedData("data/courses.json");
+    courseData
+        .then((data) => {
+            if(data[id]) {
+                data[id].offeredBy.push(req.session.user.name);
+            }
+            else {
+                data[id] = {
+                    name: req.body.name,
+                    start: req.body.start,
+                    end: req.body.end,
+                    desc: req.body.desc,
+                    offeredBy: [req.session.user.name]
+                };
+            }
+            fs.writeFile("data/courses.json", JSON.stringify(data), (err) => {
+                if(err) console.log(err);
+            });
+        }); 
+    //Also update the user coursesAuthored and session variable. Happens in parallel
+    req.session.user.coursesAuthored.push(id);
+    var userData = fileops.getParsedData("data/users.json");
+    userData
+        .then((data) => {
+            for(i of data) {
+                if(i.username == req.session.user.username) {
+                    i.coursesAuthored.push(id);
+                    break;
+                }
+            }
+            fs.writeFile("data/users.json", JSON.stringify(data), (err) => {
+                if(err) console.log(err);
+                return res.redirect("/home");
+            });
+        });
+});
+
+app.get("/list_courses", (req, res) => {
+    if(!req.session.user) return res.redirect("/login");
+    var unreg = fileops.getUnregisteredCourses(req.session.user);
+    //console.log(unreg);
+    unreg
+        .then((data) => {
+            res.render("list_courses", {
+                courses: data
+            });
+        })
+});
+
+app.get("/register/:id", (req, res) => {
+    if(!req.session.user) return res.redirect("/login");
+    var course = fileops.courseDetails(req.params.id);
+    course
+        .then((data) => {
+            res.render("register", {
+                course: data,
+                id: req.params.id
+            });
+        })
+});
+
+app.post("/register", (req, res) => {
+    var id = req.body.id;
+    var fac = req.body.prof;
+    var promise = fileops.addCourse(id, fac, req.session.user);
+    promise
+        .then(() => {
+            req.session.user.coursesRegistered[id] = fac;
+            //window.alert("Course added.")
+            return res.redirect("/home");
+        })
+});
+
+app.post("/delete", (req, res) => {
+    if(!req.session.user) return res.redirect("/login");
+    var id = req.body.id;
+    var promise = fileops.removeCourse(id, req.session.user);
+    promise
+        .then(() => {
+            //window.alert("Course dropped.")
+            return res.redirect("/home");
+        });
+});
+
+app.listen(8080);
---
+++ fileops.js
@@ -0,0 +1,139 @@
+const fs = require('fs');
+const util = require('util');
+const _ = require('lodash');
+const readFile = util.promisify(fs.readFile);
+
+function getParsedData(filename) {
+    const promise = new Promise((resolve, reject) => {
+        let users = readFile(filename, "utf-8");
+        users
+            .then((data) => {
+                let Data = JSON.parse(data);
+                resolve(Data);
+            })
+    });
+    return promise;
+}
+
+function getCourses(user) {
+    const prom = new Promise((resolve, reject) => {
+        let promise = readFile("data/courses.json", "utf-8");
+        let regCourses = [];
+        let authCourses = [];
+        let courses = user.coursesRegistered;
+        let authored = user.coursesAuthored;
+        //console.log(authored);
+        promise
+            .then((data) => {
+                let courseData = JSON.parse(data);
+                for(i in courses) {
+                    let temp = courseData[i];
+                    //console.log(temp);
+                    temp.id = i;
+                    temp.offeredBy = courses[i];
+                    regCourses.push(temp);
+                    //console.log(courseData[i]);
+                }
+                for(i of authored) {
+                    let temp2 = courseData[i];
+                    temp2.id = i;
+                    temp2.offeredBy = user.name;
+                    authCourses.push(temp2);
+                }
+                //console.log(regCourses);
+                resolve([regCourses, authCourses]);
+            })
+            .catch((err) => console.log(err));  
+    });
+    return prom;
+}
+
+function getUnregisteredCourses(user) {
+    const promise = new Promise((resolve, reject) => {
+        let courses = getParsedData("data/courses.json");
+        res = [];
+        temp = {};
+        courses
+            .then((data) => {
+                //console.log(data);
+                for(i in data) {
+                    console.log(i);
+                    if(!Object.keys(user.coursesRegistered).includes(i) && !user.coursesAuthored.includes(i)) {
+                        temp = data[i];
+                        temp.id = i;
+                        res.push(temp);
+                        //console.log(temp);
+                        temp = {};
+                    }
+                }
+                resolve(res);
+            });
+    }); 
+    return promise;
+}
+
+function courseDetails(id) {
+    const promise = new Promise((resolve, reject) => {
+        let prom = readFile("data/courses.json", "utf-8");
+        prom
+            .then((data) => {
+                let courseData = JSON.parse(data);
+                let course = courseData[id];
+                resolve(course);
+            })
+            .catch((err) => console.log(err));
+    });
+    return promise;
+}
+
+function addCourse(id, prof, user) {
+    const promise =  new Promise((resolve, reject) => {
+        let users = getParsedData("data/users.json");
+        users
+            .then((data) => {
+                for(i of data) {
+                    if(i.username == user.username) {
+                        i.coursesRegistered[id] = prof;
+                        break;
+                    }
+                }
+                fs.writeFile("data/users.json", JSON.stringify(data), (err) => {
+                    if(err) console.log(err);
+                    resolve();
+                });
+            });
+    });
+    return promise;
+}
+
+function removeCourse(id, user) {
+    user.coursesRegistered = _.omit(user.coursesRegistered, id);
+    const promise = new Promise((resolve, reject) => {
+        let users = getParsedData("data/users.json");
+        users
+            .then((data) => {
+                for(i of data) {
+                    if(i.username == user.username) {
+                        //i.coursesRegistered == _.omit(i.coursesRegistered, id);
+                        i.coursesRegistered[id] = undefined;
+                        i.coursesRegistered = JSON.parse(JSON.stringify(i.coursesRegistered));
+                        console.log(id);
+                        console.log(i.coursesRegistered);
+                        break;
+                    }
+                }
+                fs.writeFile("data/users.json", JSON.stringify(data), (err) => {
+                    if(err) console.log(err);
+                    resolve();
+                });
+            });
+    });
+    return promise;
+}
+
+module.exports.getCourses = getCourses;
+module.exports.getParsedData = getParsedData;
+module.exports.courseDetails = courseDetails;
+module.exports.getUnregisteredCourses = getUnregisteredCourses;
+module.exports.addCourse = addCourse;
+module.exports.removeCourse = removeCourse;
